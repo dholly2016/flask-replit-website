47c328327d642ec74e4f3024f65ba3307741fa88
dholly2016 <148008483+dholly2016@users.noreply.github.com>
2024-05-20 17:30:42 +0000

    adding dynamic pages to each job

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-import sqlalchemy
 from sqlalchemy import create_engine, text
 import os 
 
@@ -15,6 +14,14 @@ def load_jobs_from_db():
         jobs.append(row._asdict())
     return jobs
 
+def load_job_from_db(id):
+    with engine.connect() as conn:
+      result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{"val":id})
+      rows = result.all()
+      if len(rows) == 0:
+          return None 
+      else:
+          return rows[0]._asdict()
 
-  
- 
+
+
